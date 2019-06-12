91ae97f7c394b98f451a7032984c9e539bf47418
Noskov Artem <nowasmawesome@gmail.com>
2019-06-12 11:28:44 +0500

    Added go modules

--- cookie/cookie.go
+++ cookie/cookie.go
@@ -1,10 +1,17 @@
 package cookie
 
 import (
+	"errors"
 	"fmt"
-	"github.com/gascore/gas/web"
-	"syscall/js"
 	"strings"
+	"syscall/js"
+)
+
+var (
+	// ErrCookieNotFound if cookie not found
+	ErrCookieNotFound = errors.New("cookie not found")
+	// ErrInvalidCookie if cookie is invalid
+	ErrInvalidCookie = errors.New("invalid cookie")
 )
 
 // Set set cookie by key and value
@@ -18,7 +25,7 @@ func Get(key string) (string, error) {
 	for _, cookie := range strings.Split(cookies, "; ") {
 		cookieSplit := strings.Split(cookie, "=")
 		if len(cookieSplit) != 2 {
-			return "", web.ErrInvalidCookie
+			return "", ErrInvalidCookie
 		}
 
 		if cookieSplit[0] == key {
@@ -26,5 +33,5 @@ func Get(key string) (string, error) {
 		}
 	}
 
-	return "", web.ErrCookieNotFound
+	return "", ErrCookieNotFound
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gascore/std
+
+go 1.12
+
+require github.com/gascore/gas v0.0.0-20190612053126-40808b6ceae1 // indirect
