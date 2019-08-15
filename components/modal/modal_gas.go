662604f26a1b4e44c92fbf0969aa40afda2a336a
Noskov Artem <nowasmawesome@gmail.com>
2019-08-15 11:55:10 +0500

    Edited components/modal

--- components/modal/modal_gas.go
+++ components/modal/modal_gas.go
@@ -2,7 +2,6 @@ package modal
 
 import (
 	"fmt"
-	"github.com/gascore/dom"
 	"github.com/gascore/gas"
 )
 
@@ -32,61 +31,30 @@ func (config *Config) Normalize() {
 	}
 }
 
-type Modal struct {
-	c *gas.C
-	body []interface{}
-
-	config *Config
-
-	containerStyles string
-}
-
-func (root *Modal) Render() []interface{} {
-	return gas.CL(func()interface{} { if root.config.IsActive() { return gas.NE(&gas.E{Tag:"div", Attrs: func() gas.Map { return gas.Map{"class": root.modalWindowClass(),} },},gas.NE(&gas.E{Tag:"div", Handlers: map[string]gas.Handler{"click": func(e gas.Event) {root.disable()},},Attrs: func() gas.Map { return gas.Map{"class": root.overlayClasses(),} },},),gas.NE(&gas.E{Tag:"div", Attrs: func() gas.Map { return gas.Map{"class": "modal-window_container","style": root.containerStyles,} },},root.body,),) }; return nil }(),)
-}
-
-func (root *Modal) overlayClasses() string {
-	var classIsActive string
-	if root.config.IsActive() {
-		classIsActive = "modal-window_overlay-active"
-	}
-
-	return "modal-window_overlay "+classIsActive
-}
-
-func (root *Modal) modalWindowClass() string {
-	var classIsActive string
-	if root.config.IsActive() {
-		classIsActive = "modal-window-active"
-	}
-
-	return "modal-window "+classIsActive
-}
-
-func (root *Modal) disable() {
-	if !root.config.DisableEvents {
-    	root.config.Closer()
-    }
-}
-
 func GetModal(config *Config) gas.DynamicElement {
 	config.Normalize()
+	containerStyles := fmt.Sprintf("max-height: %s; max-width: %s;", config.MaxHeight, config.MaxWidth)
 
-	root := &Modal{
-		config: config,
-		containerStyles: fmt.Sprintf("max-height: %s; max-width: %s;", config.MaxHeight, config.MaxWidth),
-	}
+	f := &gas.F{}
 
-	c := &gas.C{Root: root}
-	root.c = c
-
-	el := c.Init()
-	return func(e gas.External) *gas.E {
-		if len(e.Body) == 0 {
-			dom.ConsoleError("invalid modal body") // just warn
+	modalWindowClass := func() string {
+		var classIsActive string
+		if config.IsActive() {
+			classIsActive = "modal-window-active"
+		} else {
+			classIsActive = "modal-window-hidden"
 		}
 
-		root.body = e.Body
-		return el
+		return "modal-window "+classIsActive
+	}
+
+	disable := func() {
+		if !config.DisableEvents {
+			config.Closer()
+		}
+	}
+
+	return func(e gas.External) *gas.E {
+		return f.Init(true, func() []interface{} {return gas.CL(gas.NE(&gas.E{Tag:"div", Attrs: func() gas.Map { return gas.Map{"class": modalWindowClass(),} },},gas.NE(&gas.E{Tag:"div", Handlers: map[string]gas.Handler{"click": func(e gas.Event) {disable()},},Attrs: func() gas.Map { return gas.Map{"class": "modal-window_overlay",} },},),gas.NE(&gas.E{Tag:"div", Attrs: func() gas.Map { return gas.Map{"class": "modal-window_container","style": containerStyles,} },},e.Body,),),)})
 	}
 }
